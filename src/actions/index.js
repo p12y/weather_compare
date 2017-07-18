4e5aa0d10529ae671fb9a144c5d85617ef8b21cb
Peter Tyldesley <ptylde@gmail.com>
2017-07-18 17:29:16 +0100

    Fixed cors error

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {OWM_API_KEY} from '../config.js';
 
 export function getWeather({city, country}) {
-  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&mode=json&units=metric&appid=${OWM_API_KEY}`;
+  const url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&mode=json&units=metric&appid=${OWM_API_KEY}`;
   const request = axios.get(url);
   if (!city) {
     return {
