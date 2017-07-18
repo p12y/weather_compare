374e821cd8ae0eaefba49fd9da673cebb9476202
Peter Tyldesley <ptylde@gmail.com>
2017-07-18 14:45:15 +0100

    Install redux-promise

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider, createStore} from 'react-redux';
+import promiseMiddleware from 'redux-promise';
+import {reducer} from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, promiseMiddleware)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
